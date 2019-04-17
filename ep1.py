d538724c7e912eea985d19cc6d9a6fc6622aa86e
Maulem <henriquemmarti@gmail.com>
2019-04-17 12:53:43 -0300

    Nova intro

--- ep1.py
+++ ep1.py
@@ -98,6 +98,13 @@ Created on Tue Apr 16 16:32:14 2019
 @author: Maulem and ThiagoD
 """
 
+###Animação de entrada:
+
+
+
+
+
+
 
 
 ###Introdução ao game:
@@ -116,7 +123,44 @@ print("Você então vai tentar pedir para o professor adiar a entrega, mas para 
 print("")
 print("Então boa sorte na sua jornada {0}!".format(player))
 
-###Funções de salas
+###Cenarios gerais:
+
+def carregar_cenarios():
+    cenarios = {
+        "inicio": {
+            "titulo": "Saguao do perigo",
+            "descricao": "Voce esta no saguao de entrada do insper",
+            "opcoes": {
+                "andar professor": "Tomar o elevador para o andar do professor",
+                "biblioteca": "Ir para a biblioteca"
+            }
+        },
+        "andar professor": {
+            "titulo": "Andar do desespero",
+            "descricao": "Voce chegou ao andar da sala do seu professor",
+            "opcoes": {
+                "inicio": "Tomar o elevador para o saguao de entrada",
+                "professor": "Falar com o professor"
+            }
+        },
+        "professor": {
+            "titulo": "O monstro do Python",
+            "descricao": "Voce foi pedir para o professor adiar o EP. "
+                         "O professor revelou que é um monstro disfarçado "
+                         "e devorou sua alma.",
+            "opcoes": {}
+        },
+        "biblioteca": {
+            "titulo": "Caverna da tranquilidade",
+            "descricao": "Voce esta na biblioteca",
+            "opcoes": {
+                "inicio": "Voltar para o saguao de entrada"
+            }
+        }
+    }
+    nome_cenario_atual = "inicio"
+    return cenarios, nome_cenario_atual
+    
 
 
 
