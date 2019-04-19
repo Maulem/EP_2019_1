37dd46eb96e899ce8b317ffba468f5dd79a03ad2
thiagoldavid <49211767+thiagoldavid@users.noreply.github.com>
2019-04-19 16:29:55 -0300

    Minha parte feita agora eh vc maulem

--- ep1.py
+++ ep1.py
@@ -67,10 +67,8 @@ def main():
             print("Acabaram-se suas opções! Mwo mwo mwooooo...")
             game_over = True
         else:
-
-            # Aluno B: substitua este comentário e a linha abaixo pelo código
-            # para pedir a escolha do usuário.
-            escolha = ""
+            
+            escolha = input("-")
 
             if escolha in opcoes:
                 nome_cenario_atual = escolha
