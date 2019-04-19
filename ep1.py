4d5131f35b49630e7d62811ae4ae395270ea8f0b
Maulem <henriquemmarti@gmail.com>
2019-04-19 11:57:52 -0300

    Base do jogo funcionando

--- ep1.py
+++ ep1.py
@@ -4,94 +4,9 @@
 # - aluno A: Henrique Mualem Marti, henriquemm3@al.insper.edu.br
 # - aluno B: Thiago Lopes, thiagod@al.insper.edu.br
 
-###Exemplo do professor(acho que nao eh obrigatorio seguir):
-"""
-def carregar_cenarios():
-    cenarios = {
-        "inicio": {
-            "titulo": "Saguao do perigo",
-            "descricao": "Voce esta no saguao de entrada do insper",
-            "opcoes": {
-                "andar professor": "Tomar o elevador para o andar do professor",
-                "biblioteca": "Ir para a biblioteca"
-            }
-        },
-        "andar professor": {
-            "titulo": "Andar do desespero",
-            "descricao": "Voce chegou ao andar da sala do seu professor",
-            "opcoes": {
-                "inicio": "Tomar o elevador para o saguao de entrada",
-                "professor": "Falar com o professor"
-            }
-        },
-        "professor": {
-            "titulo": "O monstro do Python",
-            "descricao": "Voce foi pedir para o professor adiar o EP. "
-                         "O professor revelou que é um monstro disfarçado "
-                         "e devorou sua alma.",
-            "opcoes": {}
-        },
-        "biblioteca": {
-            "titulo": "Caverna da tranquilidade",
-            "descricao": "Voce esta na biblioteca",
-            "opcoes": {
-                "inicio": "Voltar para o saguao de entrada"
-            }
-        }
-    }
-    nome_cenario_atual = "inicio"
-    return cenarios, nome_cenario_atual
-
-
-def main():
-    print("Na hora do sufoco!")
-    print("------------------")
-    print()
-    print("Parecia uma boa idéia: vou só jogar um pouquinho/assistir Netflix/"
-        "embaçar em geral. Amanhã eu começo o EP. Mas isso não deu certo...")
-    print()
-    print("É o dia de entregar o EP e você está muuuuito atrasado! Você está "
-        "na entrada do Insper, e quer procurar o professor para pedir um "
-        "adiamento do EP (boa sorte...)")
-    print()
-
-    cenarios, nome_cenario_atual = carregar_cenarios()
-
-    game_over = False
-    while not game_over:
-        cenario_atual = cenarios[nome_cenario_atual]
-
-        # Aluno A: substitua este comentário pelo código para imprimir 
-        # o cenário atual.
-
-        opcoes = cenario_atual['opcoes']
-        if len(opcoes) == 0:
-            print("Acabaram-se suas opções! Mwo mwo mwooooo...")
-            game_over = True
-        else:
-
-            # Aluno B: substitua este comentário e a linha abaixo pelo código
-            # para pedir a escolha do usuário.
-            escolha = ""
-
-            if escolha in opcoes:
-                nome_cenario_atual = escolha
-            else:
-                print("Sua indecisão foi sua ruína!")
-                game_over = True
-
-    print("Você morreu!")
-
-
-# Programa principal.
-if __name__ == "__main__":
-    main()
-"""
-
-###Vou começar nosso codigo aqui thiago mano
 
 """
-NKO Enterprises Presents: Choices game
+NKO Enterprises Presents: Escape Insper
 
 Created on Tue Apr 16 16:32:14 2019
 
@@ -142,7 +57,7 @@ def carregar_cenarios():
             }
         },
         "predio 1": {
-                "titulo:": "Saguao de entrada do predio antigo(predio 1)",
+                "titulo": "Saguao de entrada do predio antigo(predio 1)",
                 "descricao": "Voce esta no saguao de entrada do predio 1",
                 "opcoes": {
                         "inicio": "Voltar pra rua",
@@ -183,18 +98,18 @@ def carregar_cenarios():
 cenarios, cenario_atual = carregar_cenarios()
 
 
-###Funcoes dos cenarios do jogo
+###Jogo central
 
 game_over = False
 while not game_over:
-    cenario_atual = cenarios["cenario_atual"]
+    cenario_atual = cenarios[cenario_atual]
     
     print("Voce esta no(a) {0}".format(cenario_atual["titulo"]))
     print(cenario_atual["descricao"])
-    print("Suas opcoes são: {0}".format(cenario_atual["o      
+    print("Suas opcoes são: {0}".format(cenario_atual["opcoes"]))     
     
 
-    opcoes = cenario_atual["opcoes"]
+    opcoes = cenario_atual["opcoes"] 
     if len(opcoes) == 0:
         print("Acabaram-se suas opções! Mwo mwo mwooooo...")
         game_over = True
@@ -202,10 +117,10 @@ while not game_over:
 
     # Aluno B: substitua este comentário e a linha abaixo pelo código
     # para pedir a escolha do usuário.
-        escolha = ""
+        escolha = input("-")
 
     if escolha in opcoes:
-        nome_cenario_atual = escolha
+        cenario_atual = escolha
     else:
         print("Sua indecisão foi sua ruína!")
         game_over = True
